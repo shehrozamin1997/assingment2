da132d5fb8e497f9875ab888b30cef432aaa9044
shehrozamin1997 <shehrozamin786@gmail.com>
2024-03-07 10:26:58 +0500

    commit

---
+++ app.js
@@ -0,0 +1,264 @@
+
+
+// CHT 04
+// Q1
+// var userName = "Muhammad Wajahagt"
+// var age = "30"
+// var customer = "wajahat"
+
+// var statement = "<h1>"+ userName +"</h1>"+ "<h1>"+ age +"</h1>"+ "<h1>"+ customer +"</h1>"
+
+// document.write(statement)
+
+// Q2
+// ligal
+// var my_variable = "wajahat"
+// var $my_variable = "hello"
+// var _my_variable = "usman"
+// var my_variable_example = "talha"
+// var myVariableExample = "shehereyar" //need to use this
+// illigal
+// var 1myVariable = "wajahat"
+// var @my_variable = "hello"
+// var ++my_variable = "usman"
+// var %my_variable = "talha"
+// var #my_variable = "shehereyar"
+
+// Q3
+// var heading = "Rules for naming JS varibales"
+// var rules = "<h1>"+heading+"</h1>"
+// document.write(rules)
+
+// var number = 123
+// var $my_variable = "wajahat"
+// var _my_variable = "talha"
+
+// document.write("<h1>"+number+", "+$my_variable+", "+_my_variable+"</h1>")
+
+// var $my_variable = "$gloves"
+// var _my_variable = "_talha"
+// var name = "name"
+
+// document.write("<h1>"+name+", "+$my_variable+", "+_my_variable+"</h1>")
+
+
+// var nonsensitive = "<s>"+"user Name"+"</s>"
+// var sensitive = "<u>"+"userName"+"</u>"+" for best practice we use this case called camelCase"
+// var statement = "Variable names are case sensitive don't use "+nonsensitive+" use this "+sensitive 
+
+// document.write(statement)
+
+// alert(123)
+// console.log(window.alert)
+// var alert = "<p>"+"JS Keywords"+"</p>"
+
+// document.write(alert)
+// console.log(window.alert)
+
+// Chapter 5
+
+// Q1
+// var num = 3 + 2
+// document.write(num)
+
+// Q2
+// var num = 3 + 2
+// var num1 = 3 - 2
+// var num2 = 3 / 2
+// var num3 = 3 * 2
+// document.write(num+", <br>" +num1+", <br>" +num2+", <br>" +num3)
+
+// Q3
+// var num = 5
+
+// var num1 = ++num + 7;
+// num1--;
+// var num2 = num1 / 3
+
+// var num3 = 4 % num2
+// console.log(num3)
+// document.write("The remainder is: "+num3)
+
+
+
+// Q4
+// var ticketPrice = 600
+// var buyers = 5
+// var totalCost = 600 * 5
+// console.log(totalCost)
+// var msg = "Total cost to buy 5 tickets to a movie is " +totalCost+ " PKR"
+
+
+// document.write(msg)
+
+// Q5
+// for (var i = 1; i<= 10; i++ ) {
+    
+//     document.write("4 x " + i +" = "+ + 4 * i +"<br>")
+// }
+
+// var num = 0
+// var tab = "4 x " + ++num + " = ";
+// var multi = 4 * num
+// document.write(tab + multi +"<br>")
+// var tab1 = "4 x " + ++num + " = ";
+// var multi1 = 4 * num
+// document.write(tab1 + multi1 +"<br>")
+// var tab2 = "4 x " + ++num + " = ";
+// var multi2 = 4 * num
+// document.write(tab2 + multi2 +"<br>")
+// var tab3 = "4 x " + ++num + " = ";
+// var multi3 = 4 * num
+// document.write(tab3 + multi3 +"<br>")
+// var tab4 = "4 x " + ++num + " = ";
+// var multi4 = 4 * num
+// document.write(tab4 + multi4 +"<br>")
+// var tab5 = "4 x " + ++num + " = ";
+// var multi5 = 4 * num
+// document.write(tab5 + multi5 +"<br>")
+// var tab6 = "4 x " + ++num + " = ";
+// var multi6 = 4 * num
+// document.write(tab6 + multi6 +"<br>")
+// var tab7 = "4 x " + ++num + " = ";
+// var multi7 = 4 * num
+// document.write(tab7 + multi7 +"<br>")
+// var tab8 = "4 x " + ++num + " = ";
+// var multi8 = 4 * num
+// document.write(tab8 + multi8 +"<br>")
+// var tab9 = "4 x " + ++num + " = ";
+// var multi9 = 4 * num
+// document.write(tab9 + multi9 +"<br>")
+
+// Q6
+// pending
+
+// Q7
+// var item1 = 650
+// var qtyItem1 = 3
+// var item1Total = item1 * qtyItem1
+// var item2 = 100
+// var qtyItem2 = 7
+// var item2Total = item2 * qtyItem2
+// var shippingCharges = 100
+// var subTotal = item1Total + item2Total + shippingCharges
+
+// document.write("<h1>"+"Total cost of your order "+subTotal+"</h1>")
+
+// Q8
+// var totalMarks = 980
+// var marksObtained = 804
+// var percentage = marksObtained / totalMarks *100
+// document.write("<h1>"+"The Percentage of a student is "+percentage+"</h1>")
+
+// Q9
+// var usDollars = 10
+// var saudiRiyals = 25
+// var singleDollar = 104.80
+// var singleRiyal = 28
+// var convertDollars = singleDollar * usDollars
+// var convertRiyal = singleRiyal * saudiRiyals
+// var totalAmountInRupees = convertDollars + convertRiyal
+// document.write("<h1>"+"Total Currency in PKR: "+totalAmountInRupees+"</h1>")
+
+// Q10
+// var num = 5
+// var num = num * 10
+// var num = num / 2
+// document.write("<h1>"+num+"<h1>")
+
+// Q11
+// var currentYear = 2024
+// var birthYear = 1996
+// var myAge = currentYear - birthYear
+// document.write("<h1>"+myAge+"</h1>")
+
+// Q12
+// var pi = 3.142;
+// var radius = 20;
+// var circumference = 2 * pi * radius;
+// console.log(circumference)
+// var area = pi * radius ** 2;
+// console.log(area)
+// document.write("<h1>"+"The circumference is: "+circumference+"</h1>");
+// document.write("<h1>"+"The area is: "+area+"</h1>");
+
+// Q13
+// var favSnacks = "Chocoday"
+// var myAge = 28
+// var featureAge = 60
+// var snacksAmount = 2
+// var totalSnacksNowTillDeath = (featureAge - myAge) * snacksAmount
+
+// document.write("<h1>"+"I'll need "+totalSnacksNowTillDeath+" "+ favSnacks+" to last you until the ripe old age of 60"+"</h1>")
+
+// Chapter 6
+
+// Q1
+// var num = 10
+// document.write("<h1>"+"value :" +num+"</h1><hr>")
+// ++num
+// document.write("<h1>"+"before value :" +num+"</h1>")
+// document.write("<h1>"+"after value :" +num+"</h1><hr>")
+// num++
+// document.write("<h1>"+"before value :" +num+"</h1>")
+// document.write("<h1>"+"after value :" +num+"</h1><hr>")
+// --num
+// document.write("<h1>"+"before value :" +num+"</h1>")
+// document.write("<h1>"+"after value :" +num+"</h1><hr>")
+// num--
+// document.write("<h1>"+"before value :" +num+"</h1>")
+// document.write("<h1>"+"after value :" +num+"</h1><hr>")
+
+// Q2
+// var a = 2
+// var b = 1
+// document.write("<h1>"+"a = "+a+"</h1><br>")
+// document.write("<h1>"+"b = "+b+"</h1><br>")
+
+// --a;
+// a - --b;
+// a - b + ++b;
+// a - b + b + b--;
+// document.write("<h1>"+"Result = "+a+", "+b+"</h1>")
+// console.log(a, b);
+
+// Q3
+// var userName = prompt("Enter your name:");
+// document.write("<h1>"+"Hello Welcome  "+"<q>"+ userName +"</q>"+ " to our webpage"+"</h1>");
+
+// Q4
+// var tableNumberByUser = prompt("Which Table you want please type numerics only");
+
+// for (var i = 1; i<= 10; i++ ) {
+    
+//     document.write(+tableNumberByUser+" x " + i +" = "+ + tableNumberByUser * i +"<br>")
+// }
+
+// Q5
+// var sub1 = prompt("Please write how much marks you gain in English");
+// var sub2 = prompt("Please write how much marks you gain in Urdu");
+// var sub3 = prompt("Please write how much marks you gain in Computer Science");
+
+// var eachsubMarks = 100;
+// var heading = "<h1>"+"Subject Total Marks Obtained Marks Percentage"+"</h1>";
+// document.write(heading);
+// // for English
+// var subName1 = "English";
+// var engMarks = sub1 / eachsubMarks * 100;
+// document.write("<h1>"+subName1+" | "+eachsubMarks+" | "+sub1+" | "+engMarks+"%"+"</h1><br>");
+// // for Urdu
+// var subName2 = "Urdu";
+// var urduMarks = sub2 / eachsubMarks * 100;
+// document.write("<h1>"+subName2+" | "+eachsubMarks+" | "+sub2+" | "+urduMarks+"%"+"</h1><br>"); 
+// // for Computer Science
+// var subName3 = "Computer Science";
+// var csMarks = sub3 / eachsubMarks * 100;
+// document.write("<h1>"+subName3+" | "+eachsubMarks+" | "+sub3+" | "+csMarks+"%"+"</h1><br>");
+
+// // Total
+// var total = "Total Marks and Percentage";
+// var totalMarksForAllSub = eachsubMarks * 3;
+// var totalMarksGain = +sub1 + +sub2 + +sub3;
+// var overAllMarks = totalMarksGain / totalMarksForAllSub * 100;
+
+// document.write("<h1>"+"Total Percentage "+totalMarksForAllSub+" | "+totalMarksGain+" | "+overAllMarks+"%"+"</h1><br>");
